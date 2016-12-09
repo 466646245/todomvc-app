3dc6b48ec8f6fc6517b8c583c513a8ef8f6e5bea
Shepherd.香葱 <xc466646245@gmail.com>
2016-12-09 19:54:25 +0800

    注释

--- js/app.js
+++ js/app.js
@@ -11,18 +11,24 @@
 
 		];
 		//2、添加任务
+        //思路：直接给数据todoList添加即可
 		$scope.newTask="";
 		$scope.add=function () {
 			var id;
+			//如果是空字符串，就不做任何操作
 			if(!$scope.newTask){
 				return;
 			}
+			//id的计算方式：取出数据的最后一条的id，然后再加1就是当前的id
 			if($scope.todoList.length===0){
+			    //说明数组中没有数据
 				id=0;
 			}else{
+			    //说明已经存在数据了
 				id=$scope.todoList[$scope.todoList.length-1].id+1;
 			}
 			$scope.todoList.push({id:id,name:$scope.newTask,isCompleted:false});
+			//清空文本域
 			$scope.newTask="";
 			console.log($scope.todoList);
         };
@@ -41,9 +47,12 @@
         //4. 修改任务
 		//添加类名的条件：$scope.updateId与当前元素的ID相同
 		$scope.updateId=-1;
+		//update方法，只需要让updateId与当前id，这样就可以实现编辑状态
 		$scope.update=function (id) {
 			$scope.updateId=id;
         };
+		//save方法，更新updateId的值为默认值-1，因为数据是双向绑定，
+        //所以，item.id===updateId比较的值为false，所以，就由编辑状态变为不可编辑状态
 		$scope.save=function () {
 			$scope.updateId=-1;
         };
@@ -57,6 +66,8 @@
         };
         //6.清除已完成的任务
         $scope.clearCompleted=function () {
+            //目的：将数组中，所有已经完成的任务删除掉
+            //转换思路：只需要将未完成的任务取出来，放到temp数组中，最后，在替换todoList
             var temp=[];
             for(var i=0;i<$scope.todoList.length;i++){
                 var todo=$scope.todoList[i];
@@ -65,12 +76,16 @@
                 }
             }
             $scope.todoList=temp;
+            //清除数组
+            //$scope.todoList.length=0;
+            //[].push.apply($scope.todoList,temp);
         };
         //6.1 控制清除按钮 的展示与隐藏
         $scope.isShow=function () {
             for(var i=0;i<$scope.todoList.length;i++){
                 var todo=$scope.todoList[i];
                 if(todo.isCompleted){
+                    //有已经完成的任务就返回true，让按钮显示出来
                     return true;
                 }
             }
@@ -80,6 +95,7 @@
         $scope.getCount=function () {
             var count=0;
             $scope.todoList.forEach(function (value) {
+                //value急速当前元素
                 if(!value.isCompleted){
                     count+=1;
                 }
