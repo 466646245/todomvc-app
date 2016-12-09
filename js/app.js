b8240f5fa4a966fa0c357e890e43b4dee8977f62
Shepherd.香葱 <xc466646245@gmail.com>
2016-12-09 14:51:01 +0800

    展示任务列表

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@
 		//1、展示任务列表
 		//思路：创建一个数据列表，然后通过ng-repeat指令将数据展示
 		$scope.todoList=[
-			{id:0,name:'吃饭',isCompleted:false},
+			{id: 0, name: '吃饭', isCompleted: false},
             {id: 1, name: '睡觉', isCompleted: true},
             {id: 2, name: '学习', isCompleted: false},
             {id: 3, name: '打豆豆', isCompleted: false}
