684d7e94a2cf892ad2e3d9eed8350de14b507474
Shepherd.香葱 <xc466646245@gmail.com>
2016-12-10 16:38:49 +0800

    划分模块部分完成

--- js/todoController.js
+++ js/todoController.js
@@ -4,7 +4,6 @@
 (function (angular) {
     var app1=angular.module('todoApp.todoCtrl',[]);
     app1.controller('TodoController',['$scope','$location','todoSrv',function ($scope,$location,todoSrv) {
-        console.log(todoSrv.test);
         //1、展示任务列表
         //思路：创建一个数据列表，然后通过ng-repeat指令将数据展示
         $scope.todoList=todoSrv.getData();
@@ -12,33 +11,16 @@
         //思路：直接给数据todoList添加即可
         $scope.newTask="";
         $scope.add=function () {
-            var id;
             //如果是空字符串，就不做任何操作
             if(!$scope.newTask){
                 return;
-            }
-            //id的计算方式：取出数据的最后一条的id，然后再加1就是当前的id
-            if($scope.todoList.length===0){
-                //说明数组中没有数据
-                id=0;
-            }else{
-                //说明已经存在数据了
-                id=$scope.todoList[$scope.todoList.length-1].id+1;
-            }
-            $scope.todoList.push({id:id,name:$scope.newTask,isCompleted:false});
-            //清空文本域
+            };
+            todoSrv.addData($scope.newTask);
             $scope.newTask="";
-            console.log($scope.todoList);
         };
         //3.删除一条任务
         $scope.remove=function (id) {
-            for(var i=0;i<$scope.todoList.length;i++){
-                var temp=$scope.todoList[i];
-                if(temp.id==id){
-                    $scope.todoList.splice(i,1);
-                    return;
-                }
-            }
+            todoSrv.removeData(id);
         };
 
 
@@ -53,30 +35,22 @@
         //所以，item.id===updateId比较的值为false，所以，就由编辑状态变为不可编辑状态
         $scope.save=function () {
             $scope.updateId=-1;
+            todoSrv.saveData();
         };
 
         //5.切换任务选中状态
         $scope.isCheckedAll=false;
         $scope.selectAll=function () {
-            for(var i=0;i<$scope.todoList.length;i++){
-                $scope.todoList[i].isCompleted=$scope.isCheckedAll;
-            }
+           todoSrv.selectAll($scope.isCheckedAll);
         };
+        //5.1切换单个任务的完成状态，并保存数据
+        $scope.$watch('todoList',function () {
+            todoSrv.saveData();
+        },true);
         //6.清除已完成的任务
         $scope.clearCompleted=function () {
-            //目的：将数组中，所有已经完成的任务删除掉
-            //转换思路：只需要将未完成的任务取出来，放到temp数组中，最后，在替换todoList
-            var temp=[];
-            for(var i=0;i<$scope.todoList.length;i++){
-                var todo=$scope.todoList[i];
-                if(!todo.isCompleted){
-                    temp.push(todo);
-                }
-            }
-            $scope.todoList=temp;
-            //清除数组
-            //$scope.todoList.length=0;
-            //[].push.apply($scope.todoList,temp);
+           todoSrv.clearCompleted();
+           $scope.todoList=todoSrv.getData();
         };
         //6.1 控制清除按钮 的展示与隐藏
         $scope.isShow=function () {
--- js/todoService.js
+++ js/todoService.js
@@ -8,11 +8,66 @@
             //          控制器中如果要操作数据，只需要调用服务中的相应的方法即可
             // 将数据存储在localStorage中，因此ng中没有单独提供这个服务，
             //此时，可以通过$window这个服务获取到
+            //1 获取数据
             var storage=$window.localStorage;
             var dataStr=storage.getItem('todo');
             var todoList=JSON.parse(dataStr)||[];
             this.getData=function () {
                 return todoList;
+            };
+            //保存数据
+            this.saveData=function () {
+                storage.setItem('todo',JSON.stringify(todoList));
+            };
+            //2.添加数据
+            this.addData=function (newTask) {
+                var id;
+                //id的计算方式：取出数据的最后一条的id，然后再加1就是当前的id
+                if(todoList.length===0){
+                    //说明数组中没有数据
+                    id=0;
+                }else{
+                    //说明已经存在数据了
+                    id=todoList[todoList.length-1].id+1;
+                }
+                todoList.push({id:id,name:newTask,isCompleted:false});
+                //清空文本域
+                this.saveData();
+            };
+            //3.删除数据
+            this.removeData=function (id) {
+                for(var i=0;i<todoList.length;i++){
+                    if( todoList[i].id===id){
+                        todoList.splice(i,1);
+                        this.saveData();
+                        return;
+                    }
+                }
+            };
+            //5.切换任务完成状态
+            this.selectAll=function (isCheckedAll) {
+                for(var i=0;i<todoList.length;i++){
+                    isCheckedAll?
+                        todoList[i].isCompleted=true:
+                        todoList[i].isCompleted=false;
+                }
+                this.saveData();
+            };
+            //6.清除已完成任务
+            this.clearCompleted=function () {
+                //目的：将数组中，所有已经完成的任务删除掉
+                //转换思路：只需要将未完成的任务取出来，放到temp数组中，最后，在替换todoList
+                var temp=[];
+                for(var i=0;i<todoList.length;i++){
+                    if(!todoList[i].isCompleted){
+                        temp.push(todoList[i]);
+                    }
+                }
+                todoList=temp;
+                this.saveData();
+                //清除数组
+                //todoList.length=0;
+                //[].push.apply(todoList,temp);
             };
         }])
 })(angular)
